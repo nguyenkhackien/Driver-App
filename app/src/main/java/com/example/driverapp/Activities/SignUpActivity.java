9e70cba9c9193b4f69cf597c282fcdf9619a9355
nguyenkhackien <20020679@vnu.edu.vn>
2024-04-01 02:16:59 +0700

    add image to storage firebase

--- app/src/main/java/com/example/driverapp/Activities/SignUpActivity.java
+++ app/src/main/java/com/example/driverapp/Activities/SignUpActivity.java
@@ -1,7 +1,9 @@
 package com.example.driverapp.Activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -9,6 +11,7 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -20,6 +23,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 
 public class SignUpActivity extends AppCompatActivity {
@@ -28,6 +35,10 @@ public class SignUpActivity extends AppCompatActivity {
     EditText editName,editPhoneNumber,editEmail,editPassword,editPasswordAgain;
     Driver driver;
     AppCompatButton buttonSignUp;
+
+    Uri imageUri;
+
+    final int PICK_IMAGE_REQUEST = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +49,11 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void listener(){
+        uploadImg.setOnClickListener(View-> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, PICK_IMAGE_REQUEST);
+        });
         switchToLogin.setOnClickListener(view -> {
             Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
             startActivity(intent);
@@ -51,6 +67,11 @@ public class SignUpActivity extends AppCompatActivity {
             String email = editEmail.getText().toString();
             String password = editPassword.getText().toString();
             String passwordAgain = editPasswordAgain.getText().toString();
+
+            if (imageUri == null) {
+                Toast.makeText(this, "Please upload your image!", Toast.LENGTH_SHORT).show();
+                return ;
+            }
             if (name.isEmpty()) {
                 Toast.makeText(this, "Your name must not be empty!", Toast.LENGTH_SHORT).show();
                 return;
@@ -89,7 +110,22 @@ public class SignUpActivity extends AppCompatActivity {
                 public void onComplete(@NonNull Task<AuthResult> task) {
                     if(task.isSuccessful()){
                         driver.setId(mAuth.getCurrentUser().getUid());
-                        db.getReference().child("user").child(driver.getId()).setValue(driver).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        StorageReference filePath = FirebaseStorage.getInstance().getReference("DriverImages")
+                                .child(System.currentTimeMillis() + ".jpg");
+                        StorageTask uploadTask = filePath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                String downloadUri = task.getResult().toString();
+                                driver.setDriverImageUrl(downloadUri);
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                buttonSignUp.setEnabled(true);
+                            }
+                        });
+                        db.getReference().child("user").child("drivers").child(driver.getId()).setValue(driver).addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
@@ -120,5 +156,16 @@ public class SignUpActivity extends AppCompatActivity {
 
         buttonSignUp = findViewById(R.id.buttonSignUp);
         switchToLogin = findViewById(R.id.text_switchToLogin);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            imageUri = data.getData();
+            imgProfile.setImageURI(imageUri);
+        } else {
+            Toast.makeText(this, "Error, please try again!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
